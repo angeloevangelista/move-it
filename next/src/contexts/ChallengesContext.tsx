8e6dd99415276bc3f0ea3cd4d9459fc52781aae2
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-02-28 01:41:27 -0300

    Notificando usuario de novo desafio

--- next/src/contexts/ChallengesContext.tsx
+++ next/src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 import challenges from '../../challenges.json';
 
@@ -33,6 +33,10 @@ const ChallengesContextProvider: React.FC = ({ children }) => {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
   function levelUp() {
     setLevel(level + 1);
   }
@@ -43,6 +47,14 @@ const ChallengesContextProvider: React.FC = ({ children }) => {
     const challenge = challenges[randomChallengeIndex] as IChallenge;
 
     setActiveChallenge(challenge);
+
+    new Audio('/notification.mp3').play();
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo desafio 🎉', {
+        body: `Valendo ${challenge.amount}xp!`,
+      });
+    }
   }
 
   function resetChallenge() {
